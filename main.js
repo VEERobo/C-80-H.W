3aea0dbf01bbf4aade2b33daaeda2353b105bcb2
VEERobo <71770036+VEERobo@users.noreply.github.com>
2020-10-21 18:32:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function getPara1(){
+    var inputs1=[];
+    for(var i=1;i<=6;i++){
+        inputs1.push(document.getElementById("para1_input_" +i).value);
+        inputs1.join(". ");
+        document.getElementById("show_para_1").innerHTML=inputs1.join(". ");
+    }
+}
+
+function getPara2(){
+    var inputs2=[];
+    for(var i=1;i<=6;i++){
+        inputs2.push(document.getElementById("para2_input_" +i).value);
+        inputs2.join(". ");
+        document.getElementById("show_para_2").innerHTML=inputs2.join(". ");
+    }
+}
